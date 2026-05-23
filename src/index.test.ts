ab8888b7d65aa8300efcfcd2d37fc5106343025b
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-05-22 20:27:38 -0400

    remove useless tests

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "bun:test";
-import { main } from "./index";
-
-describe("index module", () => {
-	test("main is exported as a function", () => {
-		expect(typeof main).toBe("function");
-	});
-});
